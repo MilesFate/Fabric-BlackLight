3b5a1fe1a31d651c4c7075f67019784d79b094b9
shyly <breaddy16@gmail.com>
2023-08-30 01:40:41 -0500

    Added more Items

--- src/main/java/shyly/mymod/Item/ModItems.java
+++ src/main/java/shyly/mymod/Item/ModItems.java
@@ -13,14 +13,19 @@ import shyly.mymod.myMod;
 public class ModItems {
 
     public static final Item Hat = registerItem("hat", new Item(new FabricItemSettings()));
-
+    public static final Item Jacket = registerItem("jacket", new Item(new FabricItemSettings()));
+    public static final Item Trousers = registerItem("trouser", new Item(new FabricItemSettings()));
+    public static final Item Shoes = registerItem("shoes", new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(myMod.MOD_ID, name), item);
     }
 
     public static void addItemsToGroup(){
-        addItemsToGroup(ItemGroups.TOOLS, Hat);
+        addItemsToGroup(ItemGroups.COMBAT, Hat);
+        addItemsToGroup(ItemGroups.COMBAT, Jacket);
+        addItemsToGroup(ItemGroups.COMBAT, Trousers);
+        addItemsToGroup(ItemGroups.COMBAT, Shoes);
     }
 
     public static void addItemsToGroup(ItemGroup group, Item item){

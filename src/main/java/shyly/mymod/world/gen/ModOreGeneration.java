c2082b6900e13572ee7529b456dffa2a519b6f52
shyly <breaddy16@gmail.com>
2023-09-02 11:48:07 -0500

    Added ore generation

---
+++ src/main/java/shyly/mymod/world/gen/ModOreGeneration.java
@@ -0,0 +1,16 @@
+package shyly.mymod.world.gen;
+
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.gen.GenerationStep;
+import shyly.mymod.world.ModPlacedFeatures;
+
+public class ModOreGeneration {
+    public static void generateOres() {
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.CITRINE_ORE_PLACED_KEY);
+
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.BLACKLIGHT_ORE_PLACED_KEY);
+    }
+}
--- src/main/java/shyly/mymod/world/gen/ModWorldGeneration.java
+++ src/main/java/shyly/mymod/world/gen/ModWorldGeneration.java
@@ -1,7 +1,11 @@
 package shyly.mymod.world.gen;
 
+import shyly.mymod.world.ModOrePlacement;
+
 public class ModWorldGeneration {
     public static void generateModWorldGen() {
+        ModOreGeneration.generateOres();
+
         ModTreeGeneration.generateTrees();
     }
 }
